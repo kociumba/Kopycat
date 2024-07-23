8f22b77e581901dd12b3bdcec2f99b77b4c51f18
kociumba <79542688+kociumba@users.noreply.github.com>
2024-07-24 00:49:03 +0200

    added special test cases for linux

--- gui/addFolder_test.go
+++ gui/addFolder_test.go
@@ -2,7 +2,7 @@ package gui
 
 import (
 	"path/filepath"
-	"strings"
+	"runtime"
 	"testing"
 )
 
@@ -12,9 +12,11 @@ func Test_mirrorStructure(t *testing.T) {
 		destinationVolume string
 	}
 	tests := []struct {
-		name string
-		args args
-		want string
+		name          string
+		args          args
+		want          string
+		onlyOnWindows bool
+		onlyOnLinux   bool
 	}{
 		{
 			name: "test with windows separators",
@@ -22,18 +24,42 @@ func Test_mirrorStructure(t *testing.T) {
 				origin:            `C:\Users\user\gabagool`,
 				destinationVolume: `D:\`,
 			},
-			want: strings.Trim(filepath.Clean(filepath.ToSlash("D:/Users/user/gabagool")), "\n"),
+			want:          filepath.Clean(`D:/Users/user/gabagool`),
+			onlyOnWindows: true,
 		},
 		{
 			name: "Test with not fucked up separators",
 			args: args{
 				origin:            "C:/Users/user/gabagool",
-				destinationVolume: "D:/",
+				destinationVolume: `D:\`,
 			},
-			want: strings.Trim(filepath.Clean(filepath.ToSlash("D:/Users/user/gabagool")), "\n"),
+			want:          filepath.Clean("D:/Users/user/gabagool"),
+			onlyOnWindows: true,
+		},
+		{
+			name: "test with linux volume d",
+			args: args{
+				origin:            "/home/user/gabagool",
+				destinationVolume: "/mnt/d",
+			},
+			want:        filepath.Clean("/mnt/d/home/user/gabagool"),
+			onlyOnLinux: true,
+		},
+		{
+			name: "test with linux volume e",
+			args: args{
+				origin:            "/home/user/otherstuff",
+				destinationVolume: "/mnt/e",
+			},
+			want:        filepath.Clean("/mnt/e/home/user/otherstuff"),
+			onlyOnLinux: true,
 		},
 	}
 	for _, tt := range tests {
+		// Skip tests based on the OS
+		if (tt.onlyOnWindows && runtime.GOOS != "windows") || (tt.onlyOnLinux && runtime.GOOS != "linux") {
+			continue
+		}
 		t.Run(tt.name, func(t *testing.T) {
 			if got := mirrorStructure(tt.args.origin, tt.args.destinationVolume); got != tt.want {
 				t.Errorf("mirrorStructure() = %v, want %v", got, tt.want)
